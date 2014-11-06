415eabfb7d4d17f2d852068fa97cc3bf77ed89f8
Scott Martin <smartin015@gmail.com>
2014-11-05 20:20:23 -0500

    Fixed timeout causing lights to turn off while people are still inside, also allowed distance sensor to trigger lights even in cooldown mode

--- bathroom_sensor.ino
+++ bathroom_sensor.ino
@@ -13,9 +13,15 @@ int led = 13;
 #define PIR_PIN 10
 #define LED_PIN 9
 #define AC_RF_PIN A2
+#define FLOOR_LED_PIN 2
 
 #define DIST_THRESH 250
-int ctr = 0;
+int ctr = -1;
+
+boolean light_state = true;
+#define LIGHT_OFF_NTICKS 40
+#define LIGHT_ON_NTICKS 100
+#define RF_CMD_NUM_REPEATS 30
 
 HS1527 encoder(AC_RF_PIN);
 char *address = "\xBE\xA3\x90";
@@ -26,40 +32,57 @@ void setup() {
   pinMode(led, OUTPUT);  
   pinMode(DIST_PIN, INPUT);
   pinMode(PIR_PIN, INPUT);
-  //pinMode(LED_PIN, OUTPUT);
-  Serial.begin(9600);
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(FLOOR_LED_PIN, OUTPUT);
+  
+  delay(2000);
+  overhead_lights(false);
+  delay(1000);
 }
 
 boolean ledstate = true;
 
-/*
+void cabinet_lights(boolean on) {
+ digitalWrite(LED_PIN, on); 
+}
+
+void floor_lights(boolean on) {
+ digitalWrite(FLOOR_LED_PIN, on); 
+}
+
+void overhead_lights(boolean on) {
+  // Lights toggle, so start with a known state and 
+  // use a boolean to track on/off
+  if (on != light_state) {
+    for (int i = 0; i < RF_CMD_NUM_REPEATS; i++) {
+      encoder.sendMsg(address, 0); 
+    }
+  }
+  light_state = on;
+}
+
 void loop() {
   char motion = digitalRead(PIR_PIN);
   int dist = analogRead(DIST_PIN);
   
-  if (dist > DIST_THRESH) {
-    ctr = 20; 
-  }
+  boolean motion_trigger = motion && (ctr == -LIGHT_OFF_NTICKS || ctr > 0);
+  boolean distance_trigger = (dist > DIST_THRESH);
   
-  if (ctr > 0) {
-    digitalWrite(LED_PIN, HIGH); 
-    ctr--;
+  if (distance_trigger) {
+    cabinet_lights(true); 
   } else {
-    digitalWrite(LED_PIN, LOW); 
+    cabinet_lights(false); 
   }
-  delay(200);
-}
-*/
-void loop() {
-  int i = 0;
-  //for (int i = 0; i < 3; i++) {
-    for (int j = 0; j < 3; j++) {
-      digitalWrite(LED_PIN, HIGH);
-      encoder.sendMsg(address, i);
-      digitalWrite(LED_PIN, LOW);
-    }
-    //delay(0);
-  //}
   
-  delay(2000);
+  if (motion_trigger || distance_trigger) {
+    ctr = LIGHT_ON_NTICKS; 
+    floor_lights(true);
+    overhead_lights(true);
+  } else if (ctr > -LIGHT_OFF_NTICKS) {
+    ctr--; 
+    floor_lights(false);
+    overhead_lights(false);
+  }
+  
+  delay(200);
 }
